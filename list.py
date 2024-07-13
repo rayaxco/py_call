c8cd219e0d9923696d1fbc4d854af474b9d4c9dd
rayax.912@gmail.com <rayax.912@gmail.com>
2024-07-14 01:54:04 +0530

    LIST updated

--- list.py
+++ list.py
@@ -44,12 +44,60 @@ def rev_list():
         print(li)
         #remove(value) takes a value as argument not an index , for that we use pop(index)
 
+        print(li.index('daniella')) #returns the index of a value index(value)
+
+        #index(item,start index,stop index) looks for item 'daniella' in a range of indexes
+        print(li.index('daniella',3,6))
+        print(li)
+
+        print('daniella' in li) #is item 'daniella' in the list?
+
+        print(li.count('daniella')) #counts the number of times item 'daniella' exists in the list
+
+        lis = [2, 4, 5, 6, 2, 5, 8, 3, 1, 5, 46, 48, 79, 75]
+        print(sorted(lis)) #sorted  doesnt sort the actual list , it just sorts and returns a list that is sorted, datatype similarity during sorting
+        print(lis)
+
+
+        lis2=lis.copy() #copies list lis value into lis2
+        print(lis2)
+
+        lis3=lis.reverse() #modifies the list in reverse order
+        print(lis)
+
+        lis4=lis[:] #copies the list lis to lis4
+        print(lis4)
+
+        print(li[::-1]) #only prints the list in reverse order
+        print(li)
+
+        lis5=range(0,100)#pushed values 0 to 99 into lis5
+        for item in lis5:
+            print(item)
+
+        lis6='hello'.join(' 12')
+        print(lis6)
+
+    def list_unpacking():
+        a,b,c,d,*others,e=[1,2,3,4,5,6,7,8,9]
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(others)
+        print(e)
+
+
+
+
+
     lis=[2,4,5,6,2,5,8,3,1,5,46,48,79,75]
     #access(lis)
-    mix=[1,10,10.5,'daniella',True]
+    mix=[1,10,10.5,'daniella',True,'daniella',11,102]
     #mixed(mix)
-    mutable(mix)
+    #mutable(mix)
     #different datatypes can be stored inside a list
     #datatype_of_mix(mix)
     #list_functions(mix)
+    list_unpacking()
 rev_list()

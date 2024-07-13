a93d175f37a783c70ddf759f19245847d429372e
rayax.912@gmail.com <rayax.912@gmail.com>
2024-07-13 22:56:30 +0530

    LIST completed

--- list.py
+++ list.py
@@ -23,13 +23,32 @@ def rev_list():
                 item = False
 
     def datatype_of_mix(li):
+        li.append('gracias') #adds a single element to the end of the list
+        #li.prepend('hola') No such function as prepend in python
+        li.insert(0,'hola')
         for i in li:
-            print(type(i))
+            print(type(i),':',i)
+
+    def list_functions(li):
+        li.extend([100,101,True]) #extend() adds multiple elements to the end of the list
+        print(li)
+
+        print(li.pop())  #popping the last element from the list li
+        print(li)
+
+        print(li.pop(4)) #popping the fifth element from the list
+        print(li)
+
+        li.remove(10.5) # value in the argument removed from the list
+        print(li)
+        #remove(value) takes a value as argument not an index , for that we use pop(index)
 
     lis=[2,4,5,6,2,5,8,3,1,5,46,48,79,75]
     #access(lis)
     mix=[1,10,10.5,'daniella',True]
     mixed(mix)
     mutable(mix)
+    #different datatypes can be stored inside a list
     datatype_of_mix(mix)
+    list_functions(mix)
 rev_list()
